5b5b683d83d42d2cb7a457a0e17065f7bf7b4483
Henri <rougeollehenri@gmail.com>
2025-04-10 10:10:50 +0200

    Css bulma

--- POC/src/components/NavBar.vue
+++ POC/src/components/NavBar.vue
@@ -2,10 +2,10 @@
 	<nav class="navbar" role="navigation">
 		<div class="navbar-menu">
 			<div class="navbar-start">
-				<a class="navbar-item" href="index.html">Home</a>
-				<a class="navbar-item" href="Boutique.html">Boutique</a>
-				<a class="navbar-item" href="">Classement</a>
-				<a class="navbar-item" href="">Association</a>
+				<a class="navbar-item has-text-primary" href="index.html">Home</a>
+				<a class="navbar-item has-text-primary" href="Boutique.html">Boutique</a>
+				<a class="navbar-item has-text-primary" href="">Classement</a>
+				<a class="navbar-item has-text-primary" href="">Association</a>
 			</div>
 		</div>
 	</nav>
