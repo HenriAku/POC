4c92c54f4d6e0e3b7b40c196ba641aec68d871f8
Henri <rougeollehenri@gmail.com>
2025-04-10 09:38:48 +0200

    Base

---
+++ POC/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Page from './components/Page.vue'
+import NavBar from './components/NavBar.vue'
+</script>
+
+<template>
+  <Page>
+    <template v-slot:header>
+      <NavBar></NavBar>
+    </template>
+    <template v-slot:main>
+      main
+    </template>
+    <template v-slot:footer>
+      footer
+    </template>
+  </Page>
+</template>
---
+++ POC/src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+	<nav class="navbar" role="navigation">
+		<div class="navbar-menu">
+			<div class="navbar-start">
+				<a class="navbar-item" href="index.html">Home</a>
+				<a class="navbar-item" href="Boutique.html">Boutique</a>
+				<a class="navbar-item" href="">Classement</a>
+				<a class="navbar-item" href="">Association</a>
+			</div>
+		</div>
+	</nav>
+</template>
