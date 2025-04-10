eafef7663c7a6e25d0da4e21c7c47151589efbf5
bilel <mbchriki@gmail.com>
2025-04-10 10:26:07 +0200

    boutique

---
+++ POC/src/Boutique.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Page from './components/Page.vue'
+import NavBar from './components/NavBar.vue'
+import Card from './components/Card.vue'
+
+</script>
+
+<template>
+  <Page>
+    <template v-slot:header>
+      <NavBar></NavBar>
+    </template>
+    <template v-slot:main>
+      <Card></Card>
+    </template>
+    <template v-slot:footer>
+      bilel
+    </template>
+  </Page>
+</template>
+
+<style>
+  
+</style>
---
+++ POC/src/boutique.ts
@@ -0,0 +1,5 @@
+import './assets/main.css'
+import { createApp } from 'vue'
+import Boutique from './Boutique.vue'
+
+createApp(Boutique).mount('#boutique')
