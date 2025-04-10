6d8f12ea4d3054b9844cff4817f6afecb3e5903e
Henri <rougeollehenri@gmail.com>
2025-04-10 11:38:31 +0200

    footer

--- POC/src/App.vue
+++ POC/src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
   import HelloWorld from './components/HelloWorld.vue'
   import Page from './components/Page.vue'
-  import NavBar from './components/NavBar.vue'
-  import Footer from './components/Footer.vue'
 </script>
 
 <template>
--- POC/src/components/Page.vue
+++ POC/src/components/Page.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-  import NavBar from './components/NavBar.vue'
-  import Footer from './components/Footer.vue'
+  import NavBar from './NavBar.vue'
+  import Footer from './Footer.vue'
 </script>
 
 <template>
