c84d2e6c048fc650ba48346bb49f0ec417e2998e
Henri <rougeollehenri@gmail.com>
2025-04-11 11:37:08 +0200

    projet propre

--- POC/Reste/acheter.ts
+++
@@ -1,5 +0,0 @@
-import './assets/main.css'
-import { createApp } from 'vue'
-import Acheter from './Acheter.vue'
-
-createApp(Acheter).mount('#acheter')
--- POC/Reste/boutique.ts
+++
@@ -1,5 +0,0 @@
-import './assets/main.css'
-import { createApp } from 'vue'
-import Boutique from './Boutique.vue'
-
-createApp(Boutique).mount('#boutique')
